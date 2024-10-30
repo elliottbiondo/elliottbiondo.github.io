0a30838cd74ca0d932d05e1f9dde6e96bcace346
Elliott Biondo <biondoed@ornl.gov>
2024-10-30 16:37:10 -0400

    Remove projects section

--- make.py
+++ make.py
@@ -6,16 +6,18 @@ def all_lines(html_file):
     with open(html_file, 'r') as f:
         return f.readlines()
 
-
-#projects = ["fges", "svd", "gtcadis", "turbine", "cuda", "family_tree"]
-projects = ["layered", "fges", "svd", "gtcadis"]
-
 out = all_lines("html/header.html")
-for proj in projects:
-    out += all_lines("html/projects/{0}/cover.html".format(proj))
 
-for proj in projects:
-    out += all_lines("html/projects/{0}/{0}.html".format(proj))
+## Projects section {
+##projects = ["fges", "svd", "gtcadis", "turbine", "cuda", "family_tree"]
+#projects = ["layered", "fges", "svd", "gtcadis"]
+#out += all_lines("html/projects_header.html")
+#for proj in projects:
+#    out += all_lines("html/projects/{0}/cover.html".format(proj))
+#for proj in projects:
+#    out += all_lines("html/projects/{0}/{0}.html".format(proj))
+#out += all_lines("html/projects_footer.html")
+## } Projects section
 
 out += all_lines("html/publications.html")
 out += all_lines("html/footer.html")
